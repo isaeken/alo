7eb0f81cab7db0e7326cda8891d4afa7b373fb6e
Isa Eken <hello@isaeken.com.tr>
2021-04-19 05:36:08 +0300

    updated binaries

--- bin/alo.php
+++ bin/alo.php
@@ -3,7 +3,17 @@
 
 use IsaEken\Alo\Alo;
 use IsaEken\Alo\Cli;
+use IsaEken\Alo\Exceptions\DirectoryNotExistsException;
+use IsaEken\Alo\Exceptions\FileNotExistsException;
+use IsaEken\Alo\Helpers;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-(new Cli)->route(new Alo, collect($argv)->splice(1)->toArray());
+try {
+    $cli = new Cli;
+    $alo = new Alo;
+
+    $cli->route($alo, collect($argv)->splice(1)->toArray());
+} catch (DirectoryNotExistsException| FileNotExistsException $exception) {
+    Helpers::output("ERR: File or directory not found: " . $exception->getMessage());
+}
